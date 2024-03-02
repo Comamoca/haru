0b786b60e61a790767d5b28d39fd1225b07e5d31
Comamoca <comamoca.dev@gmail.com>
2024-03-02 23:42:38 +0900

    :sparkles: (index.tsx) Remove brank line.

--- index.tsx
+++ index.tsx
@@ -3,7 +3,6 @@ export default function () {
     <>
       <div class="m-3">
         <h1 class="text-4xl text-red-600">Howdy!</h1>
-
         <div class="btn btn-primary">Button</div>
       </div>
     </>
