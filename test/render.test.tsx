e31669c21e65593c5ab18033560664e5533b4d62
Comamoca <comamoca.dev@gmail.com>
2024-03-04 02:32:28 +0900

    :green_heart: (test/render.test.tsx) Add rendered html test.

--- test/render.test.tsx
+++ test/render.test.tsx
@@ -1,14 +1,17 @@
-import { renderAll } from "../utils/render.ts";
+import { assertEquals } from "https://deno.land/std@0.218.2/assert/mod.ts";
+import { renderAll, renderModule } from "../utils/render.ts";
 import { collect } from "../utils/collect.ts";
+import { join } from "path";
+
+Deno.test("testing convert jsx to html", async () => {
+  const actual =
+    `<div class="m-3"><h1 class="text-4xl text-red-600">Howdy!</h1><div class="btn btn-primary">Button</div></div>`;
+  const expect = await renderModule(join(Deno.cwd(), "index.tsx"));
+
+  assertEquals(actual, expect);
+});
 
 Deno.test("testing render", async () => {
-  // render do process below.
-  // * collect `index.{tsx, jsx}`
-  // render to HTML
-  // render result write to file
-
-  console.log(Deno.cwd());
-
   const outdir = "out";
   const glob_array = ["**/index.tsx", "**/index.jsx"];
   const target_jsx = await collect(glob_array);
