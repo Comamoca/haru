9823b229d2d17dcb69bf484090d98bd679b5e0b5
Comamoca <comamoca.dev@gmail.com>
2024-03-04 02:29:53 +0900

    :sparkles: (load_config.ts, render.ts) Add error handling and logging.

--- utils/load_config.ts
+++ utils/load_config.ts
@@ -1,6 +1,22 @@
 import { join } from "path";
+import { gray, italic } from "fmt";
+import { is } from "unknownutil";
+import * as log from "log";
 
-const config_module = await import(join(Deno.cwd(), "haru.config.ts"));
+const config_module = await (async function () {
+  try {
+    return await import(join(Deno.cwd(), "haru.config.ts"));
+  } catch {
+  }
+})();
+
+if (is.Nullish(config_module)) {
+  const style = (text: string): string => italic(gray(text));
+  log.critical(
+    `Can not load config file. Please check ${style("haru.config.ts")}.`,
+  );
+  Deno.exit(1);
+}
 
 export const config: Config = config_module.default;
 
--- utils/render.ts
+++ utils/render.ts
@@ -3,6 +3,20 @@ import { ensureDir, exists, WalkEntry } from "fs";
 import { render } from "preact-render-to-string";
 import { bgCyan } from "fmt";
 import { config } from "./load_config.ts";
+import * as log from "log";
+
+export async function renderModule(modPath: string): Promise<string> {
+  // @ts-ignore
+  const mod = await import(modPath);
+
+  if (mod.default != undefined) {
+    // <Layout title="Site title">{}</Layout>;
+    return render(mod.default());
+  }
+
+  // TODO: throw error, Can't render module.
+  return "";
+}
 
 export async function renderAll(
   outdir: string,
@@ -10,58 +24,46 @@ export async function renderAll(
   target_jsx: WalkEntry[],
 ) {
   if (target_jsx.length == 0) {
-    console.log(bgCyan("INFO"));
-    console.log("Rendering target is not found...💤");
+    throw "Target is not found.";
   }
 
   await Promise.all(
     target_jsx.map(async (jsx_entry: WalkEntry) => {
-      // @ts-ignore
-      const mod = await import(jsx_entry.path);
+      const html = await renderModule(jsx_entry.path);
 
-      if (mod.default != undefined) {
-        // console.log(jsx_entry);
+      // convert extname `.jsx` or `.tsx` to `.html`
+      const save_filename = (entry: WalkEntry) => {
+        // console.log("to save ", relative(Deno.cwd(), entry.path));
 
-        // convert extname `.jsx` or `.tsx` to `.html`
-        const save_filename = (entry: WalkEntry) => {
-          // console.log("to save ", relative(Deno.cwd(), entry.path));
+        let rel: string = relative(Deno.cwd(), entry.path);
 
-          let rel: string = relative(Deno.cwd(), entry.path);
+        glob_array.forEach((glob) => {
+          rel = rel.replace(extname(glob), ".html");
+        });
 
-          glob_array.forEach((glob) => {
-            rel = rel.replace(extname(glob), ".html");
-          });
+        return rel;
+      };
 
-          return rel;
-        };
+      const filename = save_filename(jsx_entry);
+      const parent = join(outdir, dirname(filename));
 
-        const filename = save_filename(jsx_entry);
-        const parent = join(outdir, dirname(filename));
+      log.debug(`filenale: ${filename}`);
+      log.debug(`parent: ${parent}`);
 
-        console.log(`filenale: ${filename}`);
-        console.log(`parent: ${parent}`);
+      log.debug(
+        join(relative(Deno.cwd(), jsx_entry.path), jsx_entry.name),
+      );
 
-        if (!(await exists(parent))) {
-          await ensureDir(parent);
-        }
-
-        console.log(
-          join(relative(Deno.cwd(), jsx_entry.path), jsx_entry.name),
-          // render(<Layout title="Site title">{mod.default}</Layout>),
-        );
-
-        // -------------------------------------------------
-
-        // <Layout title="Site title">{}</Layout>;
-        const html = render(mod.default);
-
-        await Deno.writeTextFile(
-          join(outdir, filename),
-          config.embed_to_html(html),
-        );
-
-        console.log(`Save to ${join(outdir, filename)}`);
+      if (!(await exists(parent))) {
+        await ensureDir(parent);
       }
+
+      await Deno.writeTextFile(
+        join(outdir, filename),
+        config.embed_to_html(html),
+      );
+
+      log.info(`Save to ${join(outdir, filename)}`);
     }),
   );
 }
